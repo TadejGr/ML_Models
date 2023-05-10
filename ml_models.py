778e29f123dd5e9db805cda1b4124dfb7b10c204
Tadej Grobelsek <tadej.grobelsek@gmail.com>
2023-05-10 21:35:06 +0200

    Update ml_models.py

--- ml_models.py
+++ ml_models.py
@@ -1,1 +1,242 @@
+from sklearn.model_selection import train_test_split #for split the data
+from sklearn.metrics import accuracy_score  #for accuracy_score
+from sklearn.model_selection import KFold #for K-fold cross validation
+from sklearn.model_selection import cross_val_score #score evaluation
+from sklearn.model_selection import cross_val_predict #prediction
+
+X_train,X_test,y_train,y_test = train_test_split(x_data,y_data,test_size=0.3,random_state=42)
+
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression 
+
+model = LogisticRegression()
+#model.fit(X_train,y_train)
+#prediction_lr=model.predict(X_test)
+#print('The accuracy of the LR is', accuracy_score(prediction_lr,y_test))
+kfold = KFold(n_splits=10, random_state=22)
+result_lr=cross_val_score(model,X_train,y_train,cv=kfold,scoring='accuracy') #if we have small dataset we can perform cv on the entire dataset and not only on training dataset
+print('The cross validated score for LR is:', result_lr.mean())
+y_pred = cross_val_predict(model,X_test,y_test,cv=kfold)
+
+# Random Forests
+from sklearn.ensemble import RandomForestClassifier
+model = RandomForestClassifier(criterion='gini', n_estimators=700,
+                             min_samples_split=10,min_samples_leaf=1,
+                             max_features='auto',oob_score=True,
+                             random_state=1,n_jobs=-1)
+model.fit(X_train,y_train)
+prediction_rm=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the Random Forest Classifier is',round(accuracy_score(prediction_rm,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_rm=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for Random Forest Classifier is:',round(result_rm.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+# Support Vector Machines
+from sklearn.svm import SVC, LinearSVC
+
+model = SVC()
+model.fit(X_train,y_train)
+prediction_svm=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the Support Vector Machines Classifier is',round(accuracy_score(prediction_svm,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_svm=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for Support Vector Machines Classifier is:',round(result_svm.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+##knn
+from sklearn.neighbors import KNeighborsClassifier
+
+
+model = KNeighborsClassifier(n_neighbors = 4)
+model.fit(X_train,y_train)
+prediction_knn=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the K Nearst Neighbors Classifier is',round(accuracy_score(prediction_knn,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_knn=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for K Nearest Neighbors Classifier is:',round(result_knn.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+# Gaussian Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+model= GaussianNB()
+model.fit(X_train,y_train)
+prediction_gnb=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the Gaussian Naive Bayes Classifier is',round(accuracy_score(prediction_gnb,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_gnb=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for Gaussian Naive Bayes classifier is:',round(result_gnb.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+# Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+model= DecisionTreeClassifier(criterion='gini', 
+                             min_samples_split=10,min_samples_leaf=1,
+                             max_features='auto')
+model.fit(X_train,y_train)
+prediction_tree=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the DecisionTree Classifier is',round(accuracy_score(prediction_tree,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_tree=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for Decision Tree classifier is:',round(result_tree.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+from sklearn.ensemble import AdaBoostClassifier
+model= AdaBoostClassifier()
+model.fit(X_train,y_train)
+prediction_adb=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the AdaBoostClassifier is',round(accuracy_score(prediction_adb,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_adb=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for AdaBoostClassifier is:',round(result_adb.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+model= LinearDiscriminantAnalysis()
+model.fit(X_train,y_train)
+prediction_lda=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the LinearDiscriminantAnalysis is',round(accuracy_score(prediction_lda,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_lda=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for AdaBoostClassifier is:',round(result_lda.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+from sklearn.ensemble import GradientBoostingClassifier
+model= GradientBoostingClassifier()
+model.fit(X_train,y_train)
+prediction_gbc=model.predict(X_test)
+print('--------------The Accuracy of the model----------------------------')
+print('The accuracy of the Gradient Boosting Classifier is',round(accuracy_score(prediction_gbc,y_test)*100,2))
+kfold = KFold(n_splits=10, random_state=22) # k=10, split the data into 10 equal parts
+result_gbc=cross_val_score(model,all_features,Targeted_feature,cv=10,scoring='accuracy')
+print('The cross validated score for AdaBoostClassifier is:',round(result_gbc.mean()*100,2))
+y_pred = cross_val_predict(model,all_features,Targeted_feature,cv=10)
+sns.heatmap(confusion_matrix(Targeted_feature,y_pred),annot=True,fmt='3.0f',cmap="summer")
+plt.title('Confusion_matrix', y=1.05, size=15)
+
+models = pd.DataFrame({
+    'Model': ['Support Vector Machines', 'KNN', 'Logistic Regression', 
+              'Random Forest', 'Naive Bayes', 'AdaBoostClassifier', 
+              'Gradient Decent', 'Linear Discriminant Analysis', 
+              'Decision Tree'],
+    'Score': [result_svm.mean(), result_knn.mean(), result_lr.mean(), 
+              result_rm.mean(), result_gnb.mean(), result_adb.mean(), 
+              result_gbc.mean(), result_lda.mean(), result_tree.mean()]})
+models.sort_values(by='Score',ascending=False)
+
+train_X = traindf.drop("Survived", axis=1)
+train_Y=traindf["Survived"]
+test_X  = testdf.drop("PassengerId", axis=1).copy()
+train_X.shape, train_Y.shape, test_X.shape
+
+# Gradient boosting tunning
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import GradientBoostingClassifier
+model = GradientBoostingClassifier()
+param_grid = {'loss' : ["deviance"],
+              'n_estimators' : [100,200,300,400],
+              'learning_rate': [0.1, 0.05, 0.01,0.001],
+              'max_depth': [4, 8],
+              'min_samples_leaf': [100,150],
+              'max_features': [0.3, 0.2,0.1] 
+              }
+
+modelf = GridSearchCV(model,param_grid = param_grid, cv=kfold, scoring="accuracy", n_jobs= 4, verbose = 1)
+
+modelf.fit(train_X,train_Y)
+
+# Best score
+modelf.best_score_
+
+# Best Estimator
+modelf.best_estimator_
+
+# Random Forest Classifier Parameters tunning 
+model = RandomForestClassifier()
+n_estim=range(100,1000,100)
+
+## Search grid for optimal parameters
+param_grid = {"n_estimators" :n_estim}
+
+
+model_rf = GridSearchCV(model,param_grid = param_grid, cv=5, scoring="accuracy", n_jobs= 4, verbose = 1)
+
+model_rf.fit(train_X,train_Y)
+
+
+
+# Best score
+print(model_rf.best_score_)
+
+#best estimator
+model_rf.best_estimator_
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+model =LinearDiscriminantAnalysis()
+param_grid = {'tol':[0.001,0.01,.1,.2]}
+
+modell = GridSearchCV(model,param_grid = param_grid, cv=5, scoring="accuracy", n_jobs= 4, verbose = 1)
+
+modell.fit(train_X,train_Y)
+
+# Best score
+print(modell.best_score_)
+
+# Best Estimator
+modell.best_estimator_
+
+model= SVC()
+param_grid = {'kernel': ['rbf','linear'], 
+                  'gamma': [ 0.001, 0.01, 0.1, 1],
+                  'C': [1, 10, 50, 100,200,300, 1000]}
+
+modelsvm = GridSearchCV(model,param_grid = param_grid, cv=5, scoring="accuracy", n_jobs= 4, verbose = 1)
+
+modelsvm.fit(train_X,train_Y)
+
+print(modelsvm.best_estimator_)
+
+# Best score
+print(modelsvm.best_score_)
+
+# Random Forests
+from sklearn.ensemble import RandomForestClassifier
+random_forest = RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+            max_depth=None, max_features='auto', max_leaf_nodes=None,
+            min_impurity_decrease=0.0, min_impurity_split=None,
+            min_samples_leaf=1, min_samples_split=2,
+            min_weight_fraction_leaf=0.0, n_estimators=400, n_jobs=1,
+            oob_score=False, random_state=None, verbose=0,
+            warm_start=False)
+random_forest.fit(train_X, train_Y)
+Y_pred_rf = random_forest.predict(test_X)
+random_forest.score(train_X,train_Y)
+acc_random_forest = round(random_forest.score(train_X, train_Y) * 100, 2)
+
+print("Important features")
+pd.Series(random_forest.feature_importances_,train_X.columns).sort_values(ascending=True).plot.barh(width=0.8)
+print('__'*30)
+print(acc_random_forest)
 
